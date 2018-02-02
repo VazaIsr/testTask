d739b8bedebec4b9c601f1a04b1ca25caebacd22
VazaIsr <vaza.isr@gmail.com>
2018-02-02 04:14:34 +0300

    test

---
+++ auth_page/js/main.js
@@ -0,0 +1,36 @@
+var preloader = function(){    
+    $('#preloader').fadeOut('slow');
+    $('.main').removeClass('hidden');
+};
+
+var hideForm = function(){
+    $('.main-form-div').addClass('hidden');    
+    $('#main-form-greetings').fadeIn();
+};
+
+var checkForm = function(){
+    var userName = $('#main-form input[name="username"]').val();
+    var userPassword = $('#main-form input[name="password"]').val();
+    if (!userName) {
+        $('.main-form-message').empty().append('Please enter the username.');    
+    } else if (!userPassword ) {
+        $('.main-form-message').empty().append('Please enter the password.');      
+    } else if (userName != "test" || userName != "test") {
+        $('.main-form-message').empty().append('Authentication error.');
+    } else {
+        $('.main-form-message').css('color','#2C9DEC').empty().append('Authorithation successful!');
+        setTimeout( hideForm, 3000);
+    };
+};
+
+$(document).ready(function(){
+    
+    setTimeout(preloader, 5000);     
+    
+    $('#main-form-submit').click(function(event){
+        event.preventDefault();
+        $('.main-form-message').empty().append('<img class="main-form-check" src="img/check.gif"><span>Authentication is in progress.</span>');
+        setTimeout(checkForm, 5000);        
+    });
+    
+});
